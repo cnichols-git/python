8aefa14306710737a5a1e2fc1dfd712b5a1e2d24
chris <chris@localhost.localdomain>
2019-11-30 16:54:45 -0500

    learning

---
+++ hello.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+# REPL Read Evaluate Print Loop
+
+# this is me working my way through the LinuxAcademy.com Python for System Admins course
+
+# single line comment
+
+"""
+this is a block comment
+this can have multiple lines
+
+"""
+
+# adding a string
+print("Hello, World!")
+
+# make a string all lowercase
+print( "HellO WORld!".lower() )
+
+#find a character
+print ( "hello world".find ('r') )
+
+# printing specail characters
+print('')
+print( '**Printing out specailcharacters**' )
+print ( "Tab\tDelimeted" )
+print('------------')
+print ( "New\nLine" )
+print('------------')
+print ( "This string has a Slash\\Character in it" )
+print('------------')
+print( "Do you need 'quotes?' ")
+print('------------')
+print( 'You can use a \' if you escape it')
+
+# srtings
+my_string = "This is a of text stored as a variable"
+print( my_string )
+
