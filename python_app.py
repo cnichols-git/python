15d71e0fb504ca2bf5e0232e3e055a3df7c77a01
Chris Nichols <cnicols.mail@gmail.com>
2019-11-30 12:48:51 -0600

    clean up

---
+++ python_app.py
@@ -0,0 +1,9 @@
+print ('Hello, World') 
+#this is a line that should not be shown 
+#I want a python app the does the following
+
+#prints to a file that is located in my documents folder 
+#print out month day time and a gretting or MOTD 
+#it will grab the last x amount of users that have logged in
+
+ 
