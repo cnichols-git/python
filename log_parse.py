88d4d3c3a0c6ba5432a456650330b4509fb96903
Chris Nichols <cnicols.mail@gmail.com>
2021-08-26 16:48:07 -0500

    update

--- log_parse.py
+++ log_parse.py
@@ -7,8 +7,28 @@
 # Get some input from the user.
 name = input("Welcome, \n May I please have your name? \n")
 
-# check for and clean user input 
+# check for length 
 num_length = len(name) 
 
+# if input is 10 or less print it out
 if num_length <= 11:
     print("Hey",name, "let's parse a file")
+
+##
+# file handeling
+
+# Python has several functions for creating, reading, updating, and deleting files
+# The default mode is 'rt' (open for reading text)
+# open a file
+file = open("/home/chris/Documents/test_logs", "r")
+print("Below is the output from logs:\n",file.read())
+
+
+# same as above
+with open("/home/chris/Documents/test_logs", "r") as infile:
+    reader = csv.reader(infile, delimeter=",")
+    header = next(reader)
+    print(header)
+
+# create a file
+# outfile = open("", w)
