1fdce50a3cc54c1df70705c0983f0b02126de4f6
Chris Nichols <cnicols.mail@gmail.com>
2021-09-25 14:14:29 -0500

    updates

--- print_examples.py
+++ print_examples.py
@@ -2,6 +2,14 @@
 
 # cnichols - python print examples
 
-with open("/home/chris/Documents/test_logs", "r") as file:
-    for i in file:
-        print(i)
+# read each line in a file then print each line 
+# with open("/home/chris/Documents/test_logs", "r") as file:
+#     for i in file:
+#         print(i)
+
+# open a file and print it
+file = open("/home/chris/Documents/test_logs", "r")
+print(file.read())
+
+# open a file and 
+file = open("/home/chris/Documents/test_logs", "r")
